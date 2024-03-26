d02d352547ce62ac998c7846980219f22b33f81b
erroresarul <163310081+erroresarul@users.noreply.github.com>
2024-03-26 17:19:44 +0600

    add new files

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px"
+
+function toggleMenu(){
+    if(menuList.style.maxHeight = "0px"){
+        menuList.style.maxHeight ="100%";
+    }
+    else{
+        menulist.style.maxHeight = "0px"
+    }
+}
